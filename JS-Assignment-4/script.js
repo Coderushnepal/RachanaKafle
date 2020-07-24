276f5d6556d71eebb6eb40a80b7d8c644b6a8484
rachana kafle <rachanakafle32@gmail.com>
2020-07-24 22:55:42 +0545

    remove scroll bar

--- JS-Assignment-4/script.js
+++ JS-Assignment-4/script.js
@@ -8,10 +8,9 @@ ball.style.height ="25px";
 ball.style.width ="25px";
 ball.style.backgroundColor = "red";
 ball.style.borderRadius= "50%";
-ball.style.position="absolute";
-
-ball.style.top=Math.random() * (box.clientHeight - 25) + "px";
-ball.style.left=Math.random() * (box.clientWidth - 25) + "px";
+ball.style.position = "absolute";
+ball.style.top = Math.random() * (box.clientHeight - 25) + "px";
+ball.style.left = Math.random() * (box.clientWidth - 25) + "px";
 //add ball in box
 box.appendChild(ball);
 });
