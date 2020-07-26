59d1642e777dbe61c3d64fc4a3232a9d3d91b432
rachana kafle <rachanakafle32@gmail.com>
2020-07-26 07:52:08 +0545

    add validation

--- JS-Assignment-7/form.js
+++ JS-Assignment-7/form.js
@@ -5,15 +5,17 @@ function Form() {
     form.setAttribute("action","");
     form.setAttribute("method","post");
     x.appendChild(form);
-       
+    form.setAttribute("onsubmit","validate()");
+
     //heading
     var heading=document.createElement("h1");
     heading.innerHTML="Register with Us";
     heading.style.textAlign="center";
     form.appendChild(heading);
 
-    //username
-    var username = document.createTextNode("Username");;
+    //username;
+    var username=document.createElement('label');
+    username.innerHTML="Username";
     form.appendChild(username);
     var linebreak=document.createElement('br');
     form.appendChild(linebreak);
@@ -22,14 +24,25 @@ function Form() {
     input_username.setAttribute("type", "text");
     input_username.setAttribute("id", "username");
     input_username.placeholder = "Enter username";
+    // console.log(input_username)
+   
     form.appendChild(input_username);
-    var linebreak=document.createElement('br');
-    form.appendChild(linebreak);
+    // var linebreak=document.createElement('br');
+    // form.appendChild(linebreak);
     var linebreak=document.createElement('br');
     form.appendChild(linebreak);
 
+    //error message
+    var error_message=document.createElement("p");
+    error_message.innerHTML="Username must be at least 3 characters";
+    error_message.style.display="none";
+    error_message.setAttribute("id", "err_username");
+    error_message.style.color="red";
+    form.appendChild(error_message);
+
     // email
-    var email=document.createTextNode("Email");
+    var email=document.createElement("label");
+    email.innerHTML="Email";
     form.appendChild(email);
     var linebreak=document.createElement('br');
     form.appendChild(linebreak);
@@ -42,11 +55,19 @@ function Form() {
 
     var linebreak=document.createElement('br');
     form.appendChild(linebreak)
-    var linebreak=document.createElement('br');
-    form.appendChild(linebreak)
+    // var linebreak=document.createElement('br');
+    // form.appendChild(linebreak)
+
+    var error_message=document.createElement("p");
+    error_message.innerHTML="Email is not valid";
+    error_message.style.display="none";
+    error_message.setAttribute("id", "err_email");
+    error_message.style.color="red";
+    form.appendChild(error_message);
 
 //password
-    var password=document.createTextNode("password");
+    var password=document.createElement("label");
+    password.innerHTML="Password"
     form.appendChild(password);
     var linebreak=document.createElement('br');
     form.appendChild(linebreak)
@@ -58,11 +79,19 @@ function Form() {
     form.appendChild(input_password)
     var linebreak=document.createElement('br');
     form.appendChild(linebreak);
-    var linebreak=document.createElement('br');
-    form.appendChild(linebreak);
+    // var linebreak=document.createElement('br');
+    // form.appendChild(linebreak);
+
+    var error_message=document.createElement("p");
+    error_message.innerHTML="password must be atleast 6 characters";
+    error_message.style.display="none";
+    error_message.setAttribute("id", "err_password");
+    error_message.style.color="red";
+    form.appendChild(error_message);
 
 //confirm password
-    var confirm_password=document.createTextNode("Confirm Password");
+    var confirm_password=document.createElement("label");
+    confirm_password.innerHTML="Confirm Password"
     form.appendChild(confirm_password);
     var linebreak=document.createElement('br');
     form.appendChild(linebreak)
@@ -77,19 +106,28 @@ function Form() {
     var linebreak=document.createElement('br');
     form.appendChild(linebreak);
 
+    var error_message=document.createElement("p");
+    error_message.innerHTML="password do not match";
+    error_message.style.display="none";
+    error_message.setAttribute("id", "err_confirm_password");
+    error_message.style.color="red";
+    form.appendChild(error_message);  
+
     //submit button
     btn = document.createElement("button"); 
     btn.innerHTML = "Submit";
+    error_message.setAttribute("id", "btn");
     btn.style.backgroundColor="#00B2FF";
     btn.style.color="white";
     btn.style.padding="5px 70px";
     btn.style.margin="5px";
-    form.appendChild(btn);           
+    form.appendChild(btn);        
 }
 Form();
 
-
-
+// document.getElementById("btn").addEventListener("click", function(event){
+//     event.preventDefault()
+//   }); 
 
 
 
--- JS-Assignment-7/validation.js
+++ JS-Assignment-7/validation.js
@@ -1,8 +1,55 @@
-// validate.js
-// function email(value){
-//     return true if valid else return false;
-//     }
-//     function username(){
-//         //write your rules and conditions
-//     return true if valid else return false;	
-//     }
+
+    function validate() {
+        var username=document.getElementById("username");
+        var error_username=document.getElementById("err_username");
+        var email=document.getElementById("email")
+        var error_email=document.getElementById("err_email")
+        var password=document.getElementById("password");
+        var error_password=document.getElementById("err_password")
+        var confirm_password=document.getElementById("confirm_password")
+        var error_confirm_password=document.getElementById("err_confirm_password")
+
+
+        if (username.value==""||username.value.length3) {
+            // alert("No blank value allowed")
+                error_username.style.display="block"; 
+                username.style.border="3px solid red";                  
+        }
+        else {
+            error_username.style.display="none"; 
+            username.style.border="3px solid green";                    
+        }
+
+        if(email.value=="") {       
+            error_email.style.display="block"; 
+            email.style.border="3px solid red";
+        }
+        else {
+            error_email.style.display="none"; 
+             email.style.border="3px solid green";         
+            }
+        if(password.value.length<6) {
+            error_password.style.display="block";  
+            password.style.border="3px solid red";
+            }
+            else {
+            error_password.style.display="none";
+            password.style.border="3px solid green";
+           }   
+           
+        //    if (confirm_password.value="") {
+
+        //    }
+
+            if(confirm_password.value==password.value){ 
+               
+                error_confirm_password.style.display="block";  
+                 confirm_password.style.border="3px solid red"; 
+            }     
+                                          
+            else {
+                error_confirm_password.style.display="none"; 
+                confirm_password.style.border="3px solid green";
+                }    
+
+    }
