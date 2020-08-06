238b5f464d27af2823d3e8ae993fb1131266f290
rachana kafle <rachanakafle32@gmail.com>
2020-08-06 17:56:09 +0545

    add

---
+++ React/React-Assignment-3/my-app/build/precache-manifest.1446d19b8b435bd57aeb0ba8ed29a30d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ae236281ab28fd9bcd998601c86fbb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a47d3308d21c34deded",
+    "url": "/static/css/main.5aaabbfe.chunk.css"
+  },
+  {
+    "revision": "222721d8182aea7c42d5",
+    "url": "/static/js/2.9f568fbd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9f568fbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a47d3308d21c34deded",
+    "url": "/static/js/main.08927b13.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
